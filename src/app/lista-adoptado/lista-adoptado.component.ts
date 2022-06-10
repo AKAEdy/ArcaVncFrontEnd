4c68375249d2dbd54503dced93d888b276de7371
chrislucero19 <christianolucero@gmail.com>
2022-06-10 10:29:45 -0500

    NUEVO ARREGLO
    CARDS ADOPCION

--- src/app/lista-adoptado/lista-adoptado.component.ts
+++ src/app/lista-adoptado/lista-adoptado.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AdopcionControllerService } from 'app/api/adopcionController.service';
+import { AnimalesService } from 'app/api/animales.service';
 import { Adopcion } from 'app/model/adopcion';
+import { Animal } from 'app/model/animal';
 
 @Component({
   selector: 'lista-adoptado',
@@ -8,37 +10,46 @@ import { Adopcion } from 'app/model/adopcion';
   styleUrls: ['./lista-adoptado.component.css']
 })
 export class ListaAdoptadoComponent implements OnInit {
-  filterpost:any='';
-  adopcion:Adopcion[]=[];
-  dataSource: any ={};
-  constructor(private adopcionService: AdopcionControllerService) { }
+ 
+  //VARIABLE DE animal
+  public animales: Animal[] = [];
+  pagina=0;
+  tamaño=2;
+  constructor(private animalesService: AnimalesService) { }
 
   ngOnInit(): void {
-    this.getAllAdopcion();
-    this.filterpost=this.adopcion;
+    this.listarAnimales();
+  this.pagina = 0;
+    // this.getAnimalesPage(
+    //   this.paginaActual.toString(),
+    //   this.totalPorPagina.toString(),
+    //   this.busqueda
+    // );
+  }
+  listarAnimales(){
+
+    this.animalesService.getAnimalsUsingGET(this.pagina,this.tamaño).subscribe(data =>{
+       this.animales = data.content
+       console.log ("listado", data)
+    })
   }
 
-  getAllAdopcion(){
-    this.adopcionService.getAllAdopcionesUsingGET().subscribe(data=>{
-      this.adopcion = data;
-    })
-    
-}
+  next(){
+    this.pagina = this.pagina + 1;
+  console.log(this.pagina);
+  this.listarAnimales();
+  }
 
+  previous(){
 
-// updateAdopcion(adopcion: Adopcion){
-// this.adopcionService.actualizarAdocionUsingPUT
-// }
+    this.pagina = this.pagina - 1;
+    if(this.pagina < 0){
+      this.pagina = 0;
+    }
 
-// filtrar(event: Event) {
-//   for (let index = 0; index < this.adopcion.length; index++) {
-//   this.dataSource = this.adopcion[index];
-    
-//     const filtro = (event.target as HTMLInputElement).value;
-//     if(this.dataSource.adoptante.persona.nombre.toLowerCase().includes(filtro.trim().toLowerCase())){
-//       console.log(this.dataSource.adoptante.persona.nombre);
-//       (this.dataSource.adoptante.persona.nombre)
-//     }
-//   }
-// }  
+    this.listarAnimales();
+    console.log(this.pagina);
+
+  }
+
 }
