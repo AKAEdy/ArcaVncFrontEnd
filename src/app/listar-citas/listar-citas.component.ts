7f619afa122e36f0a2f2c0b6ee88721ec9317a81
chrislucero19 <christianolucero@gmail.com>
2022-06-15 22:41:18 -0500

    cruds de persona vacunas y citas
    completo

--- src/app/listar-citas/listar-citas.component.ts
+++ src/app/listar-citas/listar-citas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CitasService } from 'app/api/citas.service';
 import { Cita } from 'app/model/cita';
+import { CitaDto } from 'app/model/citaDto';
 
 @Component({
   selector: 'listar-citas',
@@ -9,6 +10,19 @@ import { Cita } from 'app/model/cita';
 })
 export class ListarCitasComponent implements OnInit {
 citas: Cita[]=[];
+cita: CitaDto={
+  estado: false,
+  fechaCita: '',
+  motivo: '',
+  nombreCliente: '',
+  servicios: []
+}
+citasid: Cita={
+  estado: false,
+  fechaCita: undefined,
+  motivo: '',
+  nombreCliente: ''
+}
 
   constructor(private citaService: CitasService) { }
 
@@ -20,6 +34,35 @@ citas: Cita[]=[];
     this.citaService.getAllCitasUsingGET().subscribe(data => {
       this.citas = data;
     })
+  }
+  updateCitas(){
+    this.citaService.modificarCitaUsingPUT(this.cita, this.citasid.id, this.citasid.veterinario.id).subscribe(data =>{
+      this.citasid = data
+      location.reload
+    })
+  }
+  getCitasById(id: number){
+    this.citaService.getCitaPorIdUsingGET(id).subscribe(data => {
+  
+      this.citasid = data
+      this.mostrarEditar()
+      console.log(id);
+    })
+  }
+  deleteCitas(id: number){
+    this.citaService.eliminarCitaUsingDELETE(id).subscribe(data => {
+      location.reload
+      console.log(id);
+    })
+
+  }
+  botonCancelar() {
+    document.getElementById('tarjeta').style.display = 'none'
+    document.getElementById('tabla').style.display = 'block'
+  }
+  mostrarEditar() {
+    document.getElementById('tarjeta').style.display = 'block'
+    document.getElementById('tabla').style.display = 'none'
   }
 
 }
--- src/app/listar-vacunas/listar-vacunas.component.ts
+++ src/app/listar-vacunas/listar-vacunas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { VacunasService } from 'app/api/vacunas.service';
 import { Vacuna } from 'app/model/vacuna';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'listar-vacunas',
@@ -8,20 +9,99 @@ import { Vacuna } from 'app/model/vacuna';
   styleUrls: ['./listar-vacunas.component.css']
 })
 export class ListarVacunasComponent implements OnInit {
-vacunas: Vacuna[] = [];
+  vacunas: Vacuna[] = [];
+  vacunasid: Vacuna = {}
   constructor(private vacunaService: VacunasService) { }
 
   ngOnInit(): void {
     this.getAllVacunas();
   }
 
-  getAllVacunas(){
-    this.vacunaService.getVacunasUsingGET().subscribe(date =>
-      {
-        this.vacunas = date;
-        console.log(this.vacunas[0]);
-        
-      })
-  }
+  getAllVacunas() {
+    this.vacunaService.getVacunasUsingGET().subscribe(data => {
+      this.vacunas = data;
+      console.log(this.vacunas[0]);
 
+    })
+  }
+  updateVacunas() {
+    if(this.vacunasid.nombre === undefined || this.vacunasid.tipo === undefined || this.vacunasid.descripcion === undefined
+      || this.vacunasid.nombre === '' || this.vacunasid.tipo === '' || this.vacunasid.descripcion === ''){
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Ingrese todos los datos!',
+      })
+    }else{
+      Swal.fire({
+        title: 'Seguro quiere realizar esta acción?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Modificar',
+        denyButtonText: `No modificar`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          this.vacunaService.updateUsingPUT5(this.vacunasid, this.vacunasid.id).subscribe(data =>{
+            Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: 'Vacuna modificada exitosamente',
+              showConfirmButton: false,
+              timer: 1500
+            })
+                location.reload();
+          })
+        } else if (result.isDenied) {
+          Swal.fire('Acción cancelada', '', 'info')
+        }
+      })
+    
+     
+    }
+    
+  }
+  getVacunasById(id: number) {
+    this.vacunaService.getByIdUsingGET5(id).subscribe(data => {
+      this.vacunasid = data
+      this.mostrarEditar()
+      console.log(id);
+
+
+    })
+  }
+  deleteVacunas(id: number) {
+    Swal.fire({
+      title: '¿Esta seguro que decea eliminar?',
+      text: "No podra revertit los cambios!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#FE3838',
+      cancelButtonColor: '#878787',
+      confirmButtonText: 'Si, eliminar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Eliminado!',
+          'Registro eliminado exitosamente.',
+          'success'
+        )
+        this.getAllVacunas();
+      }
+    })
+    this.vacunaService.deleteUsingDELETE5(id).subscribe(data => {
+
+    })
+
+  }
+  botonCancelar() {
+    document.getElementById('tarjeta').style.display = 'none'
+    document.getElementById('tabla').style.display = 'block'
+  }
+  mostrarEditar() {
+    document.getElementById('tarjeta').style.display = 'block'
+    document.getElementById('tabla').style.display = 'none'
+  }
 }
+
+
