8abe4bd883ada45db1931ad802ca4851c0de906f
Willkill1512 <95000835+Willkill1512@users.noreply.github.com>
2022-07-02 00:33:29 -0500

    SEGUIMIENTO
    Terminado

--- src/app/registrar-seguimiento/registrar-seguimiento.component.ts
+++ src/app/registrar-seguimiento/registrar-seguimiento.component.ts
@@ -5,8 +5,9 @@ import { AdoptanteControllerService } from 'app/api/adoptanteController.service'
 import { SeguimientoDeAdoptadosService } from 'app/api/seguimientoDeAdoptados.service';
 import { Adopcion } from 'app/model/adopcion';
 import { Adoptante } from 'app/model/adoptante';
+import { SeguimientoAdopcion } from 'app/model/seguimientoAdopcion';
 import { SeguimientoAdopcionDto } from 'app/model/seguimientoAdopcionDto';
-import { data } from 'jquery';
+import Swal from 'sweetalert2';
  
 @Component({
   selector: 'registrar-seguimiento',
@@ -27,14 +28,26 @@ export class RegistrarSeguimientoComponent implements OnInit {
   correoadoptante:string;
   telefonoadoptante:string;
   direcionadoptante:string;
-  fechaadopcion:string;
-  seguimientoAdopcion:SeguimientoAdopcionDto={
+  fecha = new Date().toLocaleDateString();
+  seguimientoAdopcion:SeguimientoAdopcion={
     estadoSeguimiento: false,
     fechaSeguimiento: undefined,
     mensajeSeguimiento: ''
   };
   mensaje:string;
   historial:SeguimientoAdopcionDto[]=[]
+  activos:SeguimientoAdopcion[]=[]
+  editarActivos:SeguimientoAdopcionDto={
+    estadoSeguimiento: false,
+    fechaSeguimiento: undefined,
+    mensajeSeguimiento: ''
+  }
+  bloquearEnvio:boolean = false;
+  seguimientoAdopcionDto:SeguimientoAdopcionDto={
+    estadoSeguimiento: false,
+    fechaSeguimiento: undefined,
+    mensajeSeguimiento: ''
+  }
 
   constructor( private router: Router, private adopcionService: AdopcionControllerService, private adoptanteService: AdoptanteControllerService, private seguimientoAdopcionServiceImpl: SeguimientoDeAdoptadosService) { }
 
@@ -42,6 +55,8 @@ export class RegistrarSeguimientoComponent implements OnInit {
   ngOnInit(): void {
     this.getByIdAdoptantes();
     this.historialSeguimiento();
+    this.finalizarProceso();
+    
   }
 
    //seguimiento
@@ -54,7 +69,7 @@ getByIdAdoptantes(){
   this.correoadoptante = this.adopcionid.adoptante.persona.correo
   this.telefonoadoptante=this.adopcionid.adoptante.persona.telefono
   this.direcionadoptante=this.adopcionid.adoptante.persona.direccion
-  this.fechaadopcion=this.adopcionid.fechaAdopcion
+  //this.fechaadopcion=this.adopcionid.fechaAdopcion
 
 })
   this.Irseguimiento();
@@ -64,17 +79,29 @@ Irseguimiento() {
 }
 
 enviarMensaje(){
-  var adopcion_id = parseInt (localStorage.getItem("idAdoptado"));
-  this.seguimientoAdopcion.fechaSeguimiento = new Date(this.fechaadopcion);
-  this.seguimientoAdopcionServiceImpl.crearSeguimientoUsingPOST(this.seguimientoAdopcion, adopcion_id).subscribe(data =>{
+  this.bloquearEnvio = this.activos.length>0
+  console.log(this.bloquearEnvio);
+  
+  if (!this.bloquearEnvio) {
+    var adopcion_id = parseInt (localStorage.getItem("idAdoptado"));
+    this.seguimientoAdopcionDto.fechaSeguimiento = new Date(this.fecha);
+    this.seguimientoAdopcionServiceImpl.crearSeguimientoUsingPOST(this.seguimientoAdopcionDto, adopcion_id).subscribe(data =>{
     this.seguimientoAdopcion = data
+    console.log(this.seguimientoAdopcion.id);
+    location.reload()
   })
+  }
+  else{
+    Swal.fire('Aun existen seguimientos en proceso. Finalicelos para poder iniciar un nuevo seguimiento.')
+  }
 }
 
 finalizarRespuestaAdoptante(){
   var adopcion_id = parseInt (localStorage.getItem("idAdoptado"));
-  this.seguimientoAdopcion.fechaSeguimiento = new Date(this.fechaadopcion);
-  this.seguimientoAdopcionServiceImpl.editarSeguimientoUsingPUT(this.seguimientoAdopcion, adopcion_id).subscribe(data =>{
+  this.seguimientoAdopcionDto.fechaSeguimiento = this.activos[0].fechaSeguimiento;
+  this.seguimientoAdopcionDto.mensajeSeguimiento = this.activos[0].mensajeSeguimiento;
+  this.seguimientoAdopcionDto.estadoSeguimiento = false;
+  this.seguimientoAdopcionServiceImpl.editarSeguimientoUsingPUT(this.seguimientoAdopcionDto, this.activos[0].id).subscribe(data =>{
     location.reload()
  })
 }   
@@ -83,7 +110,21 @@ historialSeguimiento(){
   var adopcion_id = parseInt (localStorage.getItem("idAdoptado"));
   this.seguimientoAdopcionServiceImpl.getAllSeguimientosTerminadosUsingGET(adopcion_id).subscribe(data =>{
     this.historial = data.seguimientos
+    console.log(this.historial.length)
   })
+}
+
+finalizarProceso(){
+  var adopcion_id = parseInt (localStorage.getItem("idAdoptado"));
+  this.seguimientoAdopcionServiceImpl.getAllSeguimientosActivosUsingGET(adopcion_id).subscribe(data =>{
+    this.activos=data.seguimientos
+    console.log(this.activos[0].id);
+    
+  })
+}
+
+regresar(){
+  this.router.navigate(['listaAdoptado'])
 }
 
 }
