554cb34d3673a3c19411e0b3d70b1f5881421957
RosaVirginiaTirado <71230040+RosaVirginiaTirado@users.noreply.github.com>
2022-07-15 18:31:40 -0500

    cambios en perfil animal
    lista vacunas,lista fichas,lista animal

--- src/app/upgrade/upgrade.component.ts
+++ src/app/upgrade/upgrade.component.ts
@@ -3,10 +3,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 
 import { AnimalesRefugioService } from 'app/api/animalesRefugio.service';
+import { CarnetsDeVacunacinService } from 'app/api/carnetsDeVacunacin.service';
 import { FichasClnicasService } from 'app/api/fichasClnicas.service';
 
 
 import { AnimalRefugioResponse } from 'app/model/animalRefugioResponse';
+import { CarnetVacunacion } from 'app/model/carnetVacunacion';
 // import { FichaClinicaDTO } from 'app/model/fichaClinicaDTO';
 import { fichaClinicaPost } from 'app/model/fichaClinicaPost';
 import { data } from 'jquery';
@@ -18,122 +20,179 @@ import Swal from 'sweetalert2';
   styleUrls: ['./upgrade.component.css']
 })
 export class UpgradeComponent implements OnInit {
-  
-   //animal: AnimalRefugioResponse[] = [];
+  //animal: AnimalRefugioResponse[] = [];
+carnetVacuna:CarnetVacunacion={};
 
-  
- animal: AnimalRefugioResponse=null;
+animal: AnimalRefugioResponse=null;
 fichaClinica:fichaClinicaPost={};
+
+constructor(private activatedRoute: ActivatedRoute, private router: Router,
+ private es:AnimalesRefugioService,private fichaClinService:FichasClnicasService,private carnet:CarnetsDeVacunacinService) { }
+
+ngOnInit() {
+
+
+ const id = this.activatedRoute.snapshot.params.id;
+ this.es.getAnimalPorIdUsingGET(id).subscribe(data =>{
+   this.animal= data.animal;
+   console.log(data,"datos de animal");
+
+
+
+
+ localStorage.setItem('animal', JSON.stringify(this.animal));
+
+
+ 
+   this.getFichaByIdAnimal(id);
+ this.getCarnetByIdAnimal(id);
   
-  constructor(private activatedRoute: ActivatedRoute, private router: Router,
-    private es:AnimalesRefugioService,private fichaClinService:FichasClnicasService) { }
-
-  ngOnInit() {
-  
-  
-    const id = this.activatedRoute.snapshot.params.id;
-    this.es.getAnimalPorIdUsingGET(id).subscribe(data =>{
-      this.animal= data.animal;
-      console.log(data,"datos de animal");
+ },
+   err => {
+     this.list();
+   }
+ );}
 
 
+ getFichaByIdAnimal(id:number){
 
+   this.fichaClinService.getFichasClinicasByAnimalIdUsingGET1(id).subscribe(data=>{
+     this.fichaClinica=data as any;
+     console.log(data,"imprimiendo valores de la fichaclinica dentro del metodo get animal");
+   });
 
-    //  localStorage.setItem('animal', JSON.stringify(this.animal));
+ }
+ getCarnetByIdAnimal(id:number){
 
+   this.carnet.getFichasClinicasByAnimalIdUsingGET(id).subscribe(data=>{
+     this.carnetVacuna=data as any;
+     console.log(data,"imprimiendo valores de la vacuna dentro del metodo get animal");
+   });
 
-    
-      this.getFichaByIdAnimal(id);
-   
-     
-    },
-      err => {
-        this.list();
-      }
-    );}
-
-
-    getFichaByIdAnimal(id:number){
-
-      this.fichaClinService.getFichasClinicasByAnimalIdUsingGET1(id).subscribe(data=>{
-        this.fichaClinica=data as any;
-        console.log(data,"imprimiendo valores de la fichaclinica dentro del metodo get animal");
-      });
-
-    }
-  list(){ 
-    this.router.navigate(['/table-list']);
-  }
-  onDelete(id: number) {
-    Swal.fire({
-      title: '¿Esta seguro que decea eliminar?',
-      text: "No podra revertir los cambios!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#FE3838',
-      cancelButtonColor: '#878787',
-      confirmButtonText: 'Si, eliminar!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          'Eliminado!',
-          'Registro eliminado exitosamente.',
-          'success'
-        )
-        this.irAtras();
-      }
-    })
-    this.es.eliminarAnimalUsingDELETE(id).subscribe(data => {
-
-    })
-
-  }
+ }
 
 
 
-  irFicha(){
-    this.router.navigate(['/registrofichaclinica']);
-  }
- irAtras(){
-    this.router.navigate(['/table-list']);
-  }
+list(){ 
+ this.router.navigate(['/table-list']);
+}
+onDelete(id: number) {
+ Swal.fire({
+   title: '¿Esta seguro que decea eliminar?',
+   text: "No podra revertir los cambios!",
+   icon: 'warning',
+   showCancelButton: true,
+   confirmButtonColor: '#FE3838',
+   cancelButtonColor: '#878787',
+   confirmButtonText: 'Si, eliminar!'
+ }).then((result) => {
+   if (result.isConfirmed) {
+     Swal.fire(
+       'Eliminado!',
+       'Registro eliminado exitosamente.',
+       'success'
+     )
+     this.irAtras();
+   }
+ })
+ this.es.eliminarAnimalUsingDELETE(id).subscribe(data => {
 
-  modificar(id: number){
-    this.es.getAnimalPorIdUsingGET(id).subscribe(data =>{
-      this.animal=data;
-    console.log("listado de animal ",data);
-    this.router.navigate (['/notifications', id]);
-    });
-  }
+ })
 
-  
-
-  irVacuna(id: number){
-    this.es.getAnimalPorIdUsingGET(id).subscribe(data =>{
-      this.animal=data;
-    console.log("listado para vacunas", data);
-    this.router.navigate (['/registroCarnet', id]);
-    });
-   
-  }
+}
 
 
-  
-  irTratamiento(id: number){
-    this.fichaClinService.getByIdUsingGET1(id).subscribe(data =>{
-      this.fichaClinica=data;
-      console.log("lista ficha para tratamiento", data);
-    this.router.navigate (['/tratamiento', id]);
-    });
-    
-     
-   
-   
-  }
+
+irFicha(){
+ this.router.navigate(['/registrofichaclinica']);
+}
+irAtras(){
+ this.router.navigate(['/table-list']);
+}
+
+modificar(id: number){
+ this.es.getAnimalPorIdUsingGET(id).subscribe(data =>{
+   this.animal=data;
+ console.log("listado de animal ",data);
+ this.router.navigate (['/notifications', id]);
+ });
+}
+
+
+
+irVacuna(id: number){
+ this.es.getAnimalPorIdUsingGET(id).subscribe(data =>{
+   this.animal=data;
+ console.log("listado para vacunas", data);
+ this.router.navigate (['/registroCarnet', id]);
+ });
+
+}
+
+
+
+irTratamiento(id: number){
+ this.fichaClinService.getByIdUsingGET1(id).subscribe(data =>{
+   this.fichaClinica=data;
+   console.log("lista ficha para tratamiento", data);
+ this.router.navigate (['/tratamiento', id]);
+ });
+ 
   
 
 
+}
 
-  }
+
+borrarFicha(id: number) {
+ Swal.fire({
+   title: '¿Esta seguro que decea eliminar?',
+   text: "No podra revertir los cambios!",
+   icon: 'warning',
+   showCancelButton: true,
+   confirmButtonColor: '#FE3838',
+   cancelButtonColor: '#878787',
+   confirmButtonText: 'Si, eliminar!'
+ }).then((result) => {
+   if (result.isConfirmed) {
+     Swal.fire(
+       'Eliminado!',
+       'Registro eliminado exitosamente.',
+       'success'
+     )
+     this.irAtras();
+   }
+ })
+ this.fichaClinService.deleteUsingDELETE1(id).subscribe(data => {
+
+ })
+
+}
 
 
+
+borrarVacuna(id: number) {
+ Swal.fire({
+   title: '¿Esta seguro que decea eliminar?',
+   text: "No podra revertir los cambios!",
+   icon: 'warning',
+   showCancelButton: true,
+   confirmButtonColor: '#FE3838',
+   cancelButtonColor: '#878787',
+   confirmButtonText: 'Si, eliminar!'
+ }).then((result) => {
+   if (result.isConfirmed) {
+     Swal.fire(
+       'Eliminado!',
+       'Registro eliminado exitosamente.',
+       'success'
+     )
+     this.irAtras();
+   }
+ })
+ this.carnet.deleteUsingDELETE(id).subscribe(data => {
+
+ })
+
+}
+}
