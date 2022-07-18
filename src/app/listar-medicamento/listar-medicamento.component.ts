0318672cbb3192c451293be0c247d3cae9f5396a
chrislucero19 <christianolucero@gmail.com>
2022-07-18 11:17:40 -0500

    medicamento
    CRUD

--- src/app/listar-medicamento/listar-medicamento.component.ts
+++ src/app/listar-medicamento/listar-medicamento.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MedicamentosService } from 'app/api/medicamentos.service';
 import { Medicamento } from 'app/model/medicamento';
-import { Router } from '@angular/router';
+import { MedicamentoDto } from 'app/model/medicamentoDto';
+import Swal from 'sweetalert2';
+
+
 
 @Component({
   selector: 'listar-medicamento',
@@ -9,49 +13,149 @@ import { Router } from '@angular/router';
   styleUrls: ['./listar-medicamento.component.css']
 })
 export class ListarMedicamentoComponent implements OnInit {
-  medicamento:Medicamento[]=[]
-  pagina=0;
-  tamaño=2;
+  medicamento: Medicamento[] = []
+  medicamentoid: Medicamento = {}
+  pagina = 0;
+  tamaño = 2;
   constructor(private medicamentoService: MedicamentosService, private router: Router) { }
 
   ngOnInit(): void {
     this.getAllMedicamentos();
-    this.pagina= 0;
+    this.pagina = 0;
   }
 
-  getAllMedicamentos(){
-    this.medicamentoService.getMedicamentosUsingGET1(this.pagina,this.tamaño).subscribe(data=>{
-this.medicamento=data.content
+  getAllMedicamentos() {
+    this.medicamentoService.getMedicamentosUsingGET1(this.pagina, this.tamaño).subscribe(data => {
+      this.medicamento = data.content
     })
   }
-
-  next(){
-    this.pagina = this.pagina + 1;
-  console.log(this.pagina);
-  this.getAllMedicamentos();
+  getIdMedicamentosById(id : number) {
+    console.log(this.medicamentoid)
+    this.medicamentoService.getByIdUsingGET3(id).subscribe(data => {
+      this.medicamentoid = data
+      this.mostrarEditar();
+    })
+   
   }
-
-  previous(){
-
-    this.pagina = this.pagina - 1;
-    if(this.pagina < 0){
-      this.pagina = 0;
+  deleteMedicamentos(id :number){
+    Swal.fire({
+      title: '¿Esta seguro que decea eliminar?',
+      text: "No podra revertir los cambios!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#FE3838',
+      cancelButtonColor: '#878787',
+      confirmButtonText: 'Si, eliminar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+    this.medicamentoService.deleteUsingDELETE3(id).subscribe(data => {
+    }),err =>{
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Error al eliminar la medicacion!',
+      })
     }
-    
+    Swal.fire(
+      'Eliminado!',
+      'Registro eliminado exitosamente.',
+      'success'
+    )
+   
     this.getAllMedicamentos();
-    console.log(this.pagina);
+  }
+})
+
+}
+
+  updateMedicamentos(id: number) {
+    if (this.medicamentoid.nombreComercial === undefined
+      ||this.medicamentoid.nombreGenerico === undefined
+      ||this.medicamentoid.cantidad === undefined
+      ||this.medicamentoid.precio === undefined
+      ||this.medicamentoid.nombreComercial === ""
+      || this.medicamentoid.nombreGenerico === "" ) {
+       Swal.fire({
+         icon: 'error',
+         title: 'Oops...',
+         text: 'Ingrese todos los datos!',
+       })
+     } else {
+       Swal.fire({
+         title: 'Seguro quiererealizar esta acción?',
+         showDenyButton: true,
+         showCancelButton: true,
+         confirmButtonText: 'Modifica',
+         denyButtonText: `No modificar`,
+       }).then((result) => {
+         /* Read more about isConfirmed, isDenied below */
+         if (result.isConfirmed) {   
+          this.medicamentoService.updateUsingPUT3(this.medicamentoid, id).subscribe(data=>{
+          
+             Swal.fire({
+               position: 'center',
+               icon: 'success',
+               title: 'Medicamento registrado exitosamente',
+               showConfirmButton: false,
+               timer: 1500
+             })
+             location.reload();
+           }, err =>{
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'el medicamento ya a sido registrado!',
+            })
+          })
+           
+        } else if (result.isDenied) {
+          Swal.fire('Acción cancelada', '', 'info')
+        } 
+       })
+     }
+    
  
   }
+    
 
-  //medicacion
-  idmedicamento(id:number){
-  
-    localStorage.setItem("idmedicamento", id.toString());
 
-    this.irmedicacion();
+mostrarEditar(){
+  document.getElementById("tarjeta").style.display = "block";
+  document.getElementById("tabla").style.display = "none";
+}
+
+botonCancelar(){
+  document.getElementById("tarjeta").style.display = "none";
+  document.getElementById("tabla").style.display = "block";
+}
+next(){
+  this.pagina = this.pagina + 1;
+  console.log(this.pagina);
+  this.getAllMedicamentos();
+}
+
+previous(){
+
+  this.pagina = this.pagina - 1;
+  if (this.pagina < 0) {
+    this.pagina = 0;
   }
 
-  irmedicacion(){
-    this.router.navigateByUrl("/medicacion");
-  }
+  this.getAllMedicamentos();
+  console.log(this.pagina);
+
+}
+
+//medicacion
+idmedicamento(id: number){
+
+  localStorage.setItem("idmedicamento", id.toString());
+
+  this.irmedicacion();
+}
+
+irmedicacion(){
+  this.router.navigateByUrl("/medicacion");
+}
+ 
 }
