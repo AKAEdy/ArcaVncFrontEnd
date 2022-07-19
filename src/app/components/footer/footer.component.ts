7f77c1fa376bb6441f8e3c3b3f7ab0b366a309fc
roy.est <morochoroy@gmail.com>
2022-07-19 00:01:28 -0500

    mod_styles_views

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  test : Date = new Date();
-  
+  currentYear : Date = new Date();
+
   constructor() { }
 
   ngOnInit() {
