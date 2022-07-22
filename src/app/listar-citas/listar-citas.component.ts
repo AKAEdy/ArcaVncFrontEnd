3019c7683ea7dd6fce4f274bc8ef033fa281fe53
Willkill1512 <95000835+Willkill1512@users.noreply.github.com>
2022-07-22 14:43:22 -0500

    Citas Lista
    Citas Lista

--- src/app/listar-citas/listar-citas.component.ts
+++ src/app/listar-citas/listar-citas.component.ts
@@ -4,6 +4,10 @@ import { VeterinariosService } from 'app/api/veterinarios.service';
 import { CitaArcaExtends } from 'app/model/citaArcaExtends';
 import { CitaDto } from 'app/model/citaDto';
 import { Veterinario } from 'app/model/veterinario';
+import {FormControl} from '@angular/forms';
+import { ServiciosService } from 'app/api/servicios.service';
+import { ServicioArcaDtoExtends } from 'app/model/servicioArcaDtoExtends';
+import { ServicioArcaDto } from 'app/model/servicioArcaDto';
 
 @Component({
   selector: 'listar-citas',
@@ -14,9 +18,18 @@ export class ListarCitasComponent implements OnInit {
 citas: CitaArcaExtends[]=[];
 veterinarios: Veterinario [] = [] 
 idVeterinario:string = 'Selecciona un veterinario...'
-fecha: string;
+fecha: string = this.dateToString(new Date());
 hora:string = 'Selecciona una hora...';
 
+horasDisponibles: Array<string> = []
+fcFechaSeleccionada = new FormControl(this.dateToString(new Date()))
+serviciosArca: Array<ServicioArcaDtoExtends> = []
+service: ServicioArcaDto={}
+servicios: ServicioArcaDtoExtends = {}
+
+//Variables para el multiple select dropdown de servicios ARCA.
+fcServicios = new FormControl('');
+nombresServicios: Array<string> = [];
 
 cita: CitaDto={
   estado: false,
@@ -28,12 +41,13 @@ cita: CitaDto={
 citasid: CitaArcaExtends={}
 
 
-  constructor(private citaService: CitasService, private veterinarioService: VeterinariosService) { }
+  constructor(private citaService: CitasService, private veterinarioService: VeterinariosService,private serviciosArcaService: ServiciosService) { }
 
   ngOnInit(): void {
     this.fechaAnterior();
     this.listarAllCitas();
     this.llamarVeterinarios();
+    this.getAllServiciosArca()
   }
 
   listarAllCitas(){
@@ -42,24 +56,10 @@ citasid: CitaArcaExtends={}
     })
   }
   updateCitas(){
-    // this.citaService.modificarCitaUsingPUT(this.cita = 
-    //   {
-    //     "estado": true,
-    //     "fechaCita": "yyyy-MM-dd HH:mm",
-    //     "motivo": "string",
-    //     "nombreCliente": "string",
-    //     "servicios": [
-    //       {
-    //         "descripcion": "string",
-    //         "id": 0,
-    //         "nombre": "string",
-    //         "precio": 0
-    //       }
-    //     ]
-    //   }, this.citasid.id, this.citasid.veterinario.id).subscribe(data =>{
-    //   this.citasid = data
-    //   location.reload
-    // })
+    this.citaService.modificarCitaUsingPUT(this.cita,this.citasid.id, this.citasid.veterinario.id).subscribe(data =>{
+      this.citasid = data
+      location.reload
+    })
   }
   getCitasById(id: number){
     this.citaService.getCitaPorIdUsingGET(id).subscribe(data => {
@@ -95,19 +95,54 @@ citasid: CitaArcaExtends={}
   }
 
   fechaAnterior(){
-    var fecha = new Date();
-    var anio = fecha.getFullYear();
-    var dia = fecha.getDate();
-    var _mes = fecha.getMonth(); //viene con valores de 0 al 11
-    var mes = ""
-    _mes = _mes + 1; //ahora lo tienes de 1 al 12
-    if (_mes < 10) //ahora le agregas un 0 para el formato date
-    {
-      mes = "0" + _mes;
-    }else {
-      mes = _mes.toString();
-    }
-    mes = anio + '-' + mes + '-' + dia;
+    let mes = this.dateToString(new Date())
     document.getElementById('fechaReserva').setAttribute('min', mes)
+
+    this.fecha = mes
+  }
+
+  dateToString(fecha: Date): string {
+    let anio = fecha.getFullYear();
+    let _dia = fecha.getDate();
+    let _mes = fecha.getMonth() + 1; //viene con valores de 0 al 11
+
+    let mes = (_mes < 10)? '0'+_mes : ''+_mes
+    let dia = (_dia < 10)? '0'+_dia : ''+_dia
+
+    return anio + '-' + mes + '-'+ dia
+  }
+
+  getHorasDisponibles(fecha: string): void {
+    this.citaService.getHorasDisponiblesUsingGET(fecha).subscribe(data => this.horasDisponibles = data.horas);
+  }
+
+  setHorasSeleccionadas(): void {
+    this.getHorasDisponibles(this.fecha)
+  }
+
+  getHoraConFormatoAmPm(hora: string): string {
+    return (Number(hora.substring(0, 2)) < 12) ? hora + ' am' : hora + ' pm'
+  }
+
+  getServiciosArcaSeleccionados(): Array<ServicioArcaDtoExtends>{
+    let serviciosSeleccionados = []
+
+    Array.from(this.fcServicios.value).forEach(servicioSelect => {
+      this.serviciosArca.map((servicio) => {
+        if(servicio.nombre == servicioSelect){
+          serviciosSeleccionados.push(servicio)
+        }
+      })
+    });
+
+    return serviciosSeleccionados
+  }
+
+  getAllServiciosArca(): void {
+    this.serviciosArcaService.getAllServiciosArcaUsingGET().subscribe(data => {
+      this.serviciosArca = data
+      //Añadimos los nombres de los servicios a la lista del multiple select dropdown.
+      this.serviciosArca.map((servicio) => this.nombresServicios.push(servicio.nombre))
+    })
   }
 }
