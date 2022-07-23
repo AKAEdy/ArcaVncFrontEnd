0607ec8c95081041bc1be3018838860d52c09e6b
Willkill1512 <95000835+Willkill1512@users.noreply.github.com>
2022-07-22 19:13:03 -0500

    citas lista
    citas lista

--- src/app/listar-citas/listar-citas.component.ts
+++ src/app/listar-citas/listar-citas.component.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CitasService } from 'app/api/citas.service';
-import { VeterinariosService } from 'app/api/veterinarios.service';
 import { CitaArcaExtends } from 'app/model/citaArcaExtends';
-import { CitaDto } from 'app/model/citaDto';
-import { Veterinario } from 'app/model/veterinario';
-import {FormControl} from '@angular/forms';
-import { ServiciosService } from 'app/api/servicios.service';
-import { ServicioArcaDtoExtends } from 'app/model/servicioArcaDtoExtends';
-import { ServicioArcaDto } from 'app/model/servicioArcaDto';
 
 @Component({
   selector: 'listar-citas',
@@ -15,90 +8,49 @@ import { ServicioArcaDto } from 'app/model/servicioArcaDto';
   styleUrls: ['./listar-citas.component.css']
 })
 export class ListarCitasComponent implements OnInit {
-citas: CitaArcaExtends[]=[];
-veterinarios: Veterinario [] = [] 
-idVeterinario:string = 'Selecciona un veterinario...'
-fecha: string = this.dateToString(new Date());
-hora:string = 'Selecciona una hora...';
-
-horasDisponibles: Array<string> = []
-fcFechaSeleccionada = new FormControl(this.dateToString(new Date()))
-serviciosArca: Array<ServicioArcaDtoExtends> = []
-service: ServicioArcaDto={}
-servicios: ServicioArcaDtoExtends = {}
-
-//Variables para el multiple select dropdown de servicios ARCA.
-fcServicios = new FormControl('');
-nombresServicios: Array<string> = [];
-
-cita: CitaDto={
-  estado: false,
-  fechaCita: '',
-  motivo: '',
-  nombreCliente: '',
-  servicios: []
-}
-citasid: CitaArcaExtends={}
+  citas: CitaArcaExtends[]=[];
+  fechaBusqueda: string = this.dateToString(new Date())
+  cedulaBusqueda: string = ""
+  citaSeleccionada:CitaArcaExtends = {}
 
 
-  constructor(private citaService: CitasService, private veterinarioService: VeterinariosService,private serviciosArcaService: ServiciosService) { }
+  constructor(private citaService: CitasService) { }
+
 
   ngOnInit(): void {
-    this.fechaAnterior();
-    this.listarAllCitas();
-    this.llamarVeterinarios();
-    this.getAllServiciosArca()
+    this.getCitasPorFecha();
   }
 
-  listarAllCitas(){
-      this.citaService.getAllCitasUsingGET().subscribe(data => {
-      this.citas = data;
-    })
-  }
-  updateCitas(){
-    this.citaService.modificarCitaUsingPUT(this.cita,this.citasid.id, this.citasid.veterinario.id).subscribe(data =>{
-      this.citasid = data
-      location.reload
-    })
-  }
-  getCitasById(id: number){
-    this.citaService.getCitaPorIdUsingGET(id).subscribe(data => {
-  
-      this.citasid = data
-     console.log(this.citasid);
-     
-    })
-  }
-  
-  deleteCitas(id: number){
-    this.citaService.eliminarCitaUsingDELETE(id).subscribe(data => {
-      location.reload
-      console.log(id);
-    })
-
-  }
-  botonCancelar() {
-    document.getElementById('tarjeta').style.display = 'none'
-    document.getElementById('tabla').style.display = 'block'
-  }
-  mostrarEditar() {
-    console.log(this.citasid.cliente.nombre);
+  getCitasPorFecha(){
+    console.log("Fecha: "+ this.fechaBusqueda);
     
-    document.getElementById('tarjeta').style.display = 'block'
-    document.getElementById('tabla').style.display = 'none'
+    if (this.fechaBusqueda != null && this.fechaBusqueda != undefined && this.fechaBusqueda != "") {
+      this.citas = []
+      this.citaService.getCitasPorFechaAgendaUsingGET(this.fechaBusqueda).subscribe(data =>{
+        this.citas = data.citas
+      })
+    }
   }
 
-  llamarVeterinarios(){
-    this.veterinarioService.getAllVeterinariosUsingGET().subscribe(data =>{
-      this.veterinarios = data
-    })
+  getCitasPorCedula(){
+    if (this.cedulaBusqueda != null && this.cedulaBusqueda != undefined && this.cedulaBusqueda != "") {
+      this.citas = []
+      this.citaService.getAllCitasActivasPorClienteUsingGET(this.cedulaBusqueda).subscribe(data => {
+        this.citas = data
+      })
+    }
   }
 
-  fechaAnterior(){
-    let mes = this.dateToString(new Date())
-    document.getElementById('fechaReserva').setAttribute('min', mes)
+  setCitaSeleccionada(cita: CitaArcaExtends){
+    this.citaSeleccionada = cita
+  }
 
-    this.fecha = mes
+  getHora(fecha:string){
+    return fecha.substring(11)
+  }
+
+  getFecha(fecha:string){
+    return fecha.substring(0,11)
   }
 
   dateToString(fecha: Date): string {
@@ -110,39 +62,5 @@ citasid: CitaArcaExtends={}
     let dia = (_dia < 10)? '0'+_dia : ''+_dia
 
     return anio + '-' + mes + '-'+ dia
-  }
-
-  getHorasDisponibles(fecha: string): void {
-    this.citaService.getHorasDisponiblesUsingGET(fecha).subscribe(data => this.horasDisponibles = data.horas);
-  }
-
-  setHorasSeleccionadas(): void {
-    this.getHorasDisponibles(this.fecha)
-  }
-
-  getHoraConFormatoAmPm(hora: string): string {
-    return (Number(hora.substring(0, 2)) < 12) ? hora + ' am' : hora + ' pm'
-  }
-
-  getServiciosArcaSeleccionados(): Array<ServicioArcaDtoExtends>{
-    let serviciosSeleccionados = []
-
-    Array.from(this.fcServicios.value).forEach(servicioSelect => {
-      this.serviciosArca.map((servicio) => {
-        if(servicio.nombre == servicioSelect){
-          serviciosSeleccionados.push(servicio)
-        }
-      })
-    });
-
-    return serviciosSeleccionados
-  }
-
-  getAllServiciosArca(): void {
-    this.serviciosArcaService.getAllServiciosArcaUsingGET().subscribe(data => {
-      this.serviciosArca = data
-      //Añadimos los nombres de los servicios a la lista del multiple select dropdown.
-      this.serviciosArca.map((servicio) => this.nombresServicios.push(servicio.nombre))
-    })
   }
 }
